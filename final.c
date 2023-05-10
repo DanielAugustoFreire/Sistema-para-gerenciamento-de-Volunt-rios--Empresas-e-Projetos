a8316b4b21b149cabdb34d6449cd2df949ac1aa8
DanielAugustoSant <danielaugustosant@hotmail.com>
2023-05-10 19:30:24 -0300

    terminando jaja

--- final.c
+++ final.c
@@ -5,7 +5,7 @@
 int main(void)
 {
     //Var Normais Inteiras
-    int opcao, i, op2, numero_vol, encontrou, cod_empresa, cod_projeto;
+    int opcao, i, numero_vol, encontrou, cod_empresa, cod_projeto;
     //Var Int TL
     int  tlVoluntairo=0, tlEmpresa=0, tlProjeto=0;
     //Var de Vet
@@ -14,11 +14,20 @@ int main(void)
     char nome_voluntario[20], nome_empresa[20], desc_projeto[100];
 
     //Vetores de strings
-    char vVoluntario[TF], vEmpresa[TF], vDesc[TF];
+    char vVoluntario[TF][20], vEmpresa[TF][20], vDesc[TF][100];
 
 
 
-    printf("############\n1-Cadastro\n2-Exclusao\n3-Lancamento\n4-Relatorio\n5-Finalizar\t\t");
+    printf("Bem-vindo!\n\n");
+
+    printf("Menu:\n");
+    printf("1. Cadastro\n");
+    printf("2. Exclusão\n");
+    printf("3. Lançamento\n");
+    printf("4. Relatório\n");
+    printf("5. Finalizar\n\n");
+
+    printf("Digite o número correspondente à opção desejada: ");
     scanf("%d", &opcao);
     while(opcao!=5)
     {
@@ -26,16 +35,23 @@ int main(void)
         {
             //Cadastro
             case 1:
-                printf("++++++++++++\n1-Voluntario\n2-Empresa\n3-Projeto\n4-Retornar\t");
-                scanf("%d", &op2);
-                switch(op2)
+                printf("\nCadastro:\n");
+                printf("1. Voluntário\n");
+                printf("2. Empresa\n");
+                printf("3. Projeto\n");
+                printf("4. Retornar\n\n");
+
+                printf("Digite o número correspondente à opção desejada: ");
+                scanf("%d", &opcao);
+                switch(opcao)
                 {
                     //Voluntario
                     case 1:
                         encontrou = 0;
-                        printf("Entre com o Numero do Voluntario:");
+                        printf("\nEntre com o Numero do Voluntario:");
                         scanf("%d", &numero_vol);
-                        printf("Entre com o nome do Voluntario");
+                        getchar();
+                        printf("\nEntre com o nome do Voluntario");
                         fgets(nome_voluntario, sizeof(nome_voluntario), stdin);
                         for(i=0;i<tlVoluntairo;i++)
                         {
@@ -43,21 +59,23 @@ int main(void)
                         }
                         if(encontrou == 1)
                         {
-                            printf("Voluntario ja cadastrado");
+                            printf("###   Voluntario ja cadastrado   ###");
                         }
                         else
                         {
-                            vVoluntario[tlVoluntairo]=nome_voluntario;
+                            strcpy(vVoluntario[tlVoluntairo], nome_voluntario);
                             vNum[tlVoluntairo]=numero_vol;
                             tlVoluntairo++;
+                            printf("\nVoluntario [%d] - %s", vNum[tlVoluntairo-1], vVoluntario[tlVoluntairo-1]);
                         }
                         break;
                     //Empresa
                     case 2:
                         encontrou = 0;
-                        printf("Entre com o Codigo da Empresa:");
+                        printf("\nEntre com o Codigo da Empresa:");
                         scanf("%d", &cod_empresa);
-                        printf("Entre com o nome da Empresa");
+                        getchar();
+                        printf("\nEntre com o nome da Empresa");
                         fgets(nome_empresa, sizeof(nome_empresa), stdin);
                         for(i=0;i<tlEmpresa;i++)
                         {
@@ -65,21 +83,23 @@ int main(void)
                         }
                         if(encontrou == 1)
                         {
-                            printf("Empresa ja cadastrado");
+                            printf("###   Empresa ja cadastrado   ###");
                         }
                         else
                         {
-                            vEmpresa[tlEmpresa] = nome_empresa;
+                            strcpy(vEmpresa[tlEmpresa], nome_empresa);
                             vCod[tlEmpresa]=cod_empresa;
                             tlEmpresa++;
+                            printf("\nEmpresa [%d] - %s", vCod[tlEmpresa-1], vEmpresa[tlEmpresa-1]);
                         }
                         break;
                     //Projeto
                     case 3:
                         encontrou=0;
-                        printf("Entre com o codigo do Projeto:");
+                        printf("\nEntre com o codigo do Projeto:");
                         scanf("%d", &cod_projeto);
-                        printf("Entre com a desc. do projeto:");
+                        getchar();
+                        printf("\nEntre com a desc. do projeto:\t\t[100CaracteresMax]");
                         fgets(desc_projeto, sizeof(desc_projeto), stdin);
                         for(i=0;i<tlProjeto;i++)
                         {
@@ -87,13 +107,14 @@ int main(void)
                         }
                         if(encontrou==1)
                         {
-                            printf("Projeto ja listado.");
+                            printf("###   Projeto ja listado.   ###");
                         }
                         else
                         {
-                            //vDesc[tlProjeto]=desc_projeto;
+                            strcpy(vDesc[tlProjeto], desc_projeto);
                             vProjeto[tlProjeto]=cod_projeto;
                             tlProjeto++;
+                            printf("Desc [%d] - %s", vProjeto[tlProjeto-1], vDesc[tlProjeto-1]);
                         }
                         break;
                     //Retornar
@@ -101,43 +122,51 @@ int main(void)
                         break;
                     default: printf("Entrada nao identificada, retornando....\n");
                 }
-
-
-
-
-
-
-
-
-
-
-
                 break;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                
             //Exclusao
-            case 2: printf("Passou 2\n\n");
+            case 2: 
+                printf("Exclusão:\n\n");
+                printf("1. Voluntário\n");
+                printf("2. Empresa\n");
+                printf("3. Projeto\n");
+                printf("4. Retornar\n\n");
+                printf("Digite o numero correspondente: ");
+                scanf("%d", &opcao);
+                switch (opcao)
+                {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4: printf("Retornando:");
+                        break;
+                    default: printf("Entrada nao identificada, retornando....\n");
+                }
                 break;
-            //Lancamento
-            case 3: printf("Passou 3\n\n");
+
+            //Lancamento de horas
+            case 3:     
+                printf("\tLancamento de Horas\n\nEntre com o numero do Projeto:");
+                scanf("%d", &cod_projeto);
+                printf("Entre com o numero do voluntario: ");
+                scanf("%d", numero_vol);
+                printf("Entre com o numero da empresa: ");
+                scanf("%d", &cod_empresa)
+
+
+
+
+
+
+
+
+
                 break;
+
+
             //Relatorio
             case 4: printf("Passou 4\n\n");
                 break;
@@ -147,7 +176,17 @@ int main(void)
 
 
 
-        printf("\n\n############\n1-Cadastro\n2-Exclusao\n3-Lancamento\n4-Relatorio\n5-Finalizar\t\t");
+        
+        printf("Bem-vindo!\n\n");
+
+        printf("Menu:\n");
+        printf("1. Cadastro\n");
+        printf("2. Exclusão\n");
+        printf("3. Lançamento de Horas\n");
+        printf("4. Relatório\n");
+        printf("5. Finalizar\n\n");
+
+        printf("Digite o número correspondente à opção desejada: ");
         scanf("%d", &opcao);
     }
 }
